afddd20b069ed6cf8e01285ff886868bb759dd61
mirxxaa <ibrahimafroz77@gmail.com>
2024-11-14 18:23:53 +0300

    final Demo

--- client/src/screens/ItemDetails.js
+++ client/src/screens/ItemDetails.js
@@ -164,18 +164,15 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "bold",
   },
-
   inTheBoxTitle: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 10,
   },
-
   whatInTheBox: {
     marginBottom: 50,
     paddingHorizontal: 10,
   },
-
   inTheBoxDetails: {
     fontWeight: "bold",
     fontSize: 16,
